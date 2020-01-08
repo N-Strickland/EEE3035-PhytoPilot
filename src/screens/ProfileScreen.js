65c9c9134b000d1a61049de23e7ec6e1e5c94005
Nathan Strickland <nathanstrickland@Nathans-MacBook-Pro.local>
2020-01-08 20:41:56 +0000

    Added profile screen and fixed logout

--- src/screens/ProfileScreen.js
+++ src/screens/ProfileScreen.js
@@ -1,22 +1,48 @@
 import React, { Component } from 'react';
-import { Text, Button } from 'react-native-elements';
+import { Text, Button, Avatar } from 'react-native-elements';
 import { View, AsyncStorage, StyleSheet } from 'react-native';
+import { Auth } from 'aws-amplify';
+
+
+let username = '';
+let email = '';
+
+Auth.currentAuthenticatedUser({
+    bypassCache: false
+}).then(user => {
+    username = user.username;
+    email = user.attributes.email;
+    console.log(username, email);
+})
+    .catch(err => console.log(err));
 
 export default class ProfileScreen extends Component {
     _signOutAsync = async () => {
         await AsyncStorage.clear()
+        Auth.signOut()
+            .then(data => console.log(data))
+            .catch(err => console.log(err));
         this.props.navigation.navigate('login');
     }
 
     render() {
         return (
             <View style={styles.profileContainer}>
-                <Button buttonStyle={styles.logoutButton} containerStyle={{ marginTop: 32, flex: 0 }} title="Log Out" onPress={this._signOutAsync} />
+                <Avatar
+                    size="xlarge"
+                    rounded
+                    title={username[0]}
+                    activeOpacity={0.7}
+                />
+                <View style={styles.infobox}>
+                    <Text style={styles.infotext}>Name: {username} </Text>
+                    <Text style={styles.infotext}>Email: {email}</Text>
+                </View>
+                <Button buttonStyle={styles.logoutButton} title="Log Out" onPress={this._signOutAsync} />
             </View>
         );
     }
 }
-
 
 const styles = StyleSheet.create({
     logoutButton: {
@@ -26,6 +52,16 @@ const styles = StyleSheet.create({
         width: 200,
     },
     profileContainer: {
-        alignItems: "center"
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center',
+        paddingTop: 30
+    },
+    infobox: {
+        padding: 30,
+    },
+    infotext: {
+        fontSize: 18,
+        padding: 5
     }
 })
